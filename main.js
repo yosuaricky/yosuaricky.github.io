5ec62876dc12ac1d37c1639702527b7134e1f506
ysricky <yosuaricky2606@gmail.com>
2021-08-31 17:41:08 +0700

    add sending form functionality using google sheet & google script, minor UI tweak

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-// javascript here~
+// send submitted form value to google sheets
+const scriptURL =
+  'https://script.google.com/macros/s/AKfycbzows9jk6sDDzGNTkbE7VAr3Au5KsEFeJPinm5HITF0aa8B4H5oTyyq2zpsoyJs37PC/exec';
+const form = document.forms['contact-form'];
+const btnSubmit = document.querySelector('.btn-submit-message');
+const btnSending = document.querySelector('.btn-sending-message');
+const myAlert = document.querySelector('.my-alert');
+
+const switchingButton = () => {
+  btnSending.classList.toggle('d-none');
+  btnSubmit.classList.toggle('d-none');
+};
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  // show sending/loading button & hide submit button
+  switchingButton();
+  fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+    .then((response) => {
+      // hide sending/loading button & show submit button
+      switchingButton();
+      // show alert message
+      myAlert.classList.toggle('d-none');
+      form.reset();
+      console.log('Success!', response);
+    })
+    .catch((error) => console.error('Error!', error.message));
+});
